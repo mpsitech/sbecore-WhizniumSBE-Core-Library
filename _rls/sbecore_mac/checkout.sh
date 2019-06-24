5ea90c3970ff3f708985d948e5c9dd648bfc5a1f
Alexander Wirthmueller <aw@mpsitech.com>
2019-06-24 17:19:26 +0200

    mac releases

---
+++ _rls/sbecore_mac/checkout.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# file checkout.sh
+# checkout script for Whiznium SBE core library, release sbecore_mac
+# author Alexander Wirthmueller
+# date created: 14 Aug 2014
+# modified: 24 Jun 2019
+
+export set SRCROOT=${WHIZROOT}/src
+
+mkdir ${SRCROOT}/sbecore
+
+cp Makefile ${SRCROOT}/sbecore/
+
+cp ../../*.h ${SRCROOT}/sbecore/
+cp ../../*.cpp ${SRCROOT}/sbecore/
